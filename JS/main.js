acb8153ba894c3598a2ebe857a658e2bb8d37cf2
UrielVazquez <cuadruoficial@gmail.com>
2025-09-12 19:46:22 -0600

    ajuste rutas

--- JS/main.js
+++ JS/main.js
@@ -30,7 +30,7 @@ function cargarProductos() {
             <img src="${producto.imagen}" alt="${producto.nombre}">
             <h3>${producto.nombre}</h3>
             <p>${producto.descripcion}</p>
-            <p>${producto.precio}</p>
+            <p>$${producto.precio}</p>
             <button class="btn-agregar-carrito" data-id="${producto.id}">Agregar al carrito</button>
             `;
 
